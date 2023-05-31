fde3d8d947632ef4dd321c5a2c1d22031bf56b04
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2023-05-31 22:52:27 +0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './css/App.scss';
-import Animate from './components/Animate';
-import Hand from '../public/images/hands.png';
 import Progress from './components/Progress';
 import { AiOutlineCheckCircle } from 'react-icons/ai';
+import { BsPower } from 'react-icons/bs';
 
 const ScanArea = () => {
   const [activeHands, setActiveHands] = useState([]);
@@ -12,21 +11,10 @@ const ScanArea = () => {
 
   const handleIconClick = (hand) => {
     if (activeHands.includes(hand)) {
-      setActiveHands(activeHands.filter((h) => h !== hand));
+      setActiveHands([]);
     } else {
-      setActiveHands([...activeHands, hand]);
+      setActiveHands(['right', 'left']);
     }
-  };
-
-  const HandIcon = ({ hand }) => {
-    const isActive = activeHands.includes(hand);
-
-    return (
-      <div onClick={() => handleIconClick(hand)}>
-        <img src={Hand} className={isActive ? 'active' : ''} />
-        {activeHands.length > 0 && <Animate key="hand" />}
-      </div>
-    );
   };
 
   useEffect(() => {
@@ -47,38 +35,23 @@ const ScanArea = () => {
     }
   }, [activeHands]);
 
-  const handleKeyDown = (event) => {
-    if (event.key) {
-      handleIconClick("right");
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
-
   return (
     <section id="finder">
       <div className="container">
         <div className="row">
-          <div className="scan-right">
-            <HandIcon hand="right" />
+          <div className={`scan-right ${activeHands.includes('right') ? 'active' : ''}`} onClick={() => handleIconClick('right')}>
+            <div className="button-content"><BsPower /></div>
           </div>
-          <div style={{ transform: 'scaleX(-1)' }} className="scan-left">
-            <HandIcon hand="left" />
+          <div className={`scan-left ${activeHands.includes('left') ? 'active' : ''}`} onClick={() => handleIconClick('left')}>
+            <div className="button-content"><BsPower /></div>
           </div>
         </div>
         {showProgress && <Progress />}
-
-          <div className={`finish ${showFinish ? 'active' : ''}`}>
-            <div className="content">
-              <AiOutlineCheckCircle />
-            </div>
+        <div className={`finish ${showFinish ? 'active' : ''}`}>
+          <div className="content">
+            <AiOutlineCheckCircle />
           </div>
+        </div>
       </div>
     </section>
   );
