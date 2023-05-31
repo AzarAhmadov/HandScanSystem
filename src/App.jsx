15c11740d312c629d6f7a4d4dd18a02ee6eb7b53
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2023-05-31 23:18:20 +0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import React, { useState, useEffect } from 'react';
 import './css/App.scss';
 import Progress from './components/Progress';
 import { AiOutlineCheckCircle } from 'react-icons/ai';
-import { BsPower } from 'react-icons/bs';
+import RedImage from '../public/images/bt_red.png';
+import GreenImage from '../public/images/bt_green.png';
 
 const ScanArea = () => {
   const [activeHands, setActiveHands] = useState([]);
   const [showProgress, setShowProgress] = useState(false);
   const [showFinish, setShowFinish] = useState(false);
+  const [buttonImage, setButtonImage] = useState(RedImage);
 
   const handleIconClick = (hand) => {
     if (activeHands.includes(hand)) {
       setActiveHands([]);
+      setButtonImage(RedImage);
     } else {
       setActiveHands(['right', 'left']);
+      setButtonImage(GreenImage);
     }
   };
 
@@ -26,7 +30,6 @@ const ScanArea = () => {
         const finishTimer = setTimeout(() => {
           setShowFinish(false);
         }, 4700);
-        setActiveHands([]);
 
         return () => clearTimeout(finishTimer);
       }, 4700);
@@ -40,18 +43,20 @@ const ScanArea = () => {
       <div className="container">
         <div className="row">
           <div className={`scan-right ${activeHands.includes('right') ? 'active' : ''}`} onClick={() => handleIconClick('right')}>
-            <div className="button-content"><BsPower /></div>
+            <img src={buttonImage} alt="Button" className="button-image" />
           </div>
           <div className={`scan-left ${activeHands.includes('left') ? 'active' : ''}`} onClick={() => handleIconClick('left')}>
-            <div className="button-content"><BsPower /></div>
+            <img src={buttonImage} alt="Button" className="button-image" />
           </div>
         </div>
         {showProgress && <Progress />}
-        <div className={`finish ${showFinish ? 'active' : ''}`}>
-          <div className="content">
-            <AiOutlineCheckCircle />
+        {showFinish && (
+          <div className="finish active">
+            <div className="content">
+              <AiOutlineCheckCircle />
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </section>
   );
